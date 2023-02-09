01dcb3c30dfe061c2d2395c296bc3ffd528b1a3b
enotowitch <en.enotowitch@gmail.com>
2023-02-09 12:41:58 +0200

    +login, register, auth

---
+++ controllers/UserController.js
@@ -0,0 +1,69 @@
+import UserModel from "../models/User.js"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import config from "config"
+
+function myError(err, res) {
+	console.log(err)
+	return res.status(501).json(err) // todo delete err to front on production
+}
+
+export const register = async (req, res) => {
+	try {
+
+		const pass = req.body.password
+		const passwordHash = await bcrypt.hash(pass, 10)
+
+		const doc = await new UserModel({
+			email: req.body.email,
+			password: passwordHash
+		})
+
+		const user = await doc.save()
+		const { password, ...userData } = user._doc // * trim password
+		const token = jwt.sign({ _id: user._id }, config.get("jwtKey"))
+
+		res.json({ ...userData, token })
+
+	} catch (err) {
+		myError(err, res)
+	}
+}
+export const login = async (req, res) => {
+	try {
+
+		const user = await UserModel.findOne({ email: req.body.email })
+		if (!user) {
+			myError(err, res)
+		}
+
+		const pass = req.body.password
+		const isValidPass = await bcrypt.compare(pass, user._doc.password)
+		if (!isValidPass) {
+			myError(err, res)
+		}
+
+		const { password, ...userData } = user._doc // * trim password
+		const token = jwt.sign({ _id: user._id }, config.get("jwtKey"))
+
+		res.json({ ...userData, token })
+
+	} catch (err) {
+		myError(err, res)
+	}
+}
+
+export const auth = async (req, res) => {
+	try {
+		const { token } = req.params
+		const decoded = jwt.verify(token, config.get("jwtKey"))
+
+		const user = await UserModel.findById(decoded._id)
+		const { password, ...userData } = user._doc // * trim password
+
+		res.json(userData)
+
+	} catch (err) {
+		myError(err, res)
+	}
+}
---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express"
+import config from "config"
+import mongoose from "mongoose"
+import cors from "cors"
+
+import * as UserController from "./controllers/UserController.js"
+
+mongoose.connect("mongodb+srv://enotowitch:qwerty123@cluster0.9tnodta.mongodb.net/dbNameHere?retryWrites=true&w=majority")
+	.then(console.log("DB OK")).catch(err => console.log("DB ERR", err))
+
+// ! use
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const PORT = config.get("port")
+app.listen(PORT, (err) => err ? console.log("SERVER ERR", err) : console.log(`SERVER OK, PORT: ${PORT}`))
+
+// ! routes
+app.post("/register", UserController.register)
+app.post("/login", UserController.login)
+app.post("/auth/:token", UserController.auth)
